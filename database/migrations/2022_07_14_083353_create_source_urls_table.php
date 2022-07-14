3ed66a9973c330e01128409d3897cb9f34308a68
Shubham chugh <chugh.shubham12@gmail.com>
2022-07-14 14:05:51 +0530

    basic migration

---
+++ database/migrations/2022_07_14_083353_create_source_urls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('source_urls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('is_scraped');
+            $table->string('value')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('source_urls');
+    }
+};
