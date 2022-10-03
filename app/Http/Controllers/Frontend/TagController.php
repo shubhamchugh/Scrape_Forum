4fbc51fa32a36df04d180954458b3f304f9ba9f0
ShubhamChugh OS 22.04 <chugh.shubham12@gmail.com>
2022-10-03 13:41:18 +0000

    commit from jaipur

--- app/Http/Controllers/Frontend/TagController.php
+++ app/Http/Controllers/Frontend/TagController.php
@@ -5,43 +5,45 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Spatie\Tags\Tag;
 
 class TagController extends Controller
 {
     public function TagShow($tag)
     {
-        $tagName = implode('-', array_map('ucfirst', explode('-', $tag)));
+        $tagName = Tag::where('')
       
         $related_posts = NUll;
         $tags = Null;
         $theme_path_home = 'themes.' . config('value.THEME_NAME') . '.content.home';
 
         $posts = Post::withAnyTags([$tagName], 'QA')
-            ->with(['user','tags'])
-            ->select('slug','post_title','post_description','updated_at')
+            ->with(['user','tags',])
             ->where('status','publish')
-            ->paginate(10);
+            ->select('id','user_id','slug','post_title','post_description','updated_at')
+            ->limit(10)
+            ->simplePaginate(10);
 
-                if ($posts->isEmpty()) {
+            if ($posts->isEmpty()) {
                 return abort(404);
-                }
-              
+            }
+            
         foreach ($posts as $post) {
-            dd($post->tags);
+            
             if (!($post->tags)->isEmpty()) {
                 foreach ($post->tags as $tag) {
                     $tags['name'][] =  $tag->name;  
                     $tags['slug'][] =  $tag->slug;  
                     $tag_type = $tag->type; 
                     }
-                    dd($tag->name);
+                    
                     $related_posts  = Post::withAnyTags([$tag->name], $tag_type)
                     ->select('slug','post_title')
                     ->where('status','publish')
                     ->limit(config('value.RELATED_POSTS_COUNT'))
                     ->get();
             }
-            }
+        }
 
             return view($theme_path_home,[
                 'posts' => $posts,
